7584ee92f7d2356f3edd2da8b8972497c213048d
Leebonggu <leebonggu12@hanmail.net>
2023-04-28 00:02:55 +0900

    feat: controller basic

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { v1 } from 'uuid';
 
 @Injectable() // nest 프로젝트 어디에서든 사용가능
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards() {
+    return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: v1(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
+  }
+}
